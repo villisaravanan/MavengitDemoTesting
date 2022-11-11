f6d449a91a07c84ba090239abb4bcfb76556b64e
Villi_Saravanan <Villi_Saravanan@ip-10-242-0-58.ec2.internal>
2022-11-11 16:16:14 +0530

    First commit

---
+++ MavengitDemoTesting/src/test/java/Com/RunTest/OutputTesting.java
@@ -0,0 +1,11 @@
+package Com.RunTest;
+
+public class OutputTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Villi Saravanan");
+
+	}
+
+}
