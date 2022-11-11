cf4b0320f9b605da9db73208a61f75b554615d4d
Villi_Saravanan <Villi_Saravanan@ip-10-242-0-58.ec2.internal>
2022-11-11 17:02:27 +0530

    Second file added

---
+++ MavengitDemoTesting/src/test/java/Com/RunTest/Secondoutput.java
@@ -0,0 +1,10 @@
+package Com.RunTest;
+
+public class Secondoutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second Villi Saravanan");
+	}
+
+}
