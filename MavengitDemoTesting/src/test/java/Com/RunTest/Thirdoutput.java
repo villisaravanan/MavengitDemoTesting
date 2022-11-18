18aaf85a4f239975d34df510c1687ace5b9ea87c
Villi_Saravanan <Villi_Saravanan@ip-10-242-0-38.ec2.internal>
2022-11-18 16:32:47 +0530

    third new file is added

---
+++ MavengitDemoTesting/src/test/java/Com/RunTest/Thirdoutput.java
@@ -0,0 +1,10 @@
+package Com.RunTest;
+
+public class Thirdoutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Third Villi Saravanan");
+	}
+
+}
